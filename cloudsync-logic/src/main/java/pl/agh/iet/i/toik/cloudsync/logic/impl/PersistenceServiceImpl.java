dcb1d56a222c13bf534fc89884a44cf729965843
Jakub Tokaj <jakub.tokaj@boost.no>
2014-05-11 21:04:19 +0200

    Implemented PresistenceService using simple java serialization

--- cloudsync-logic/src/main/java/pl/agh/iet/i/toik/cloudsync/logic/impl/PersistenceServiceImpl.java
+++ cloudsync-logic/src/main/java/pl/agh/iet/i/toik/cloudsync/logic/impl/PersistenceServiceImpl.java
@@ -6,40 +6,113 @@ import org.springframework.stereotype.Service;
 
 import pl.agh.iet.i.toik.cloudsync.logic.PersistenceService;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class PersistenceServiceImpl implements PersistenceService {
+    private static final String DB_FILE_NAME = "database.db";
 	private static Logger logger = LoggerFactory.getLogger(PersistenceServiceImpl.class);
+
+    private Map<String, Map<String, Integer>> sequences;
+    private Map<String, Map<String, Object>> database;
+
+    public PersistenceServiceImpl(){
+        loadDatabase();
+    }
 
 	@Override
 	public <T> void put(String prefix, String id, T obj) {
 		logger.info("Putting key: " + id + ", value:" + obj + ", prefix:" + prefix);
-		// TODO(kubicz): Implement this.
+
+        if (!database.containsKey(prefix)){
+            database.put(prefix, new HashMap<String, Object>());
+        }
+        database.get(prefix).put(id, obj);
+		saveDatabase();
 	}
 
 	@Override
 	public <T> T get(String prefix, String id) {
 		logger.info("Getting key: " + id + ", prefix:" + prefix);
-		return null;
-		// TODO(kubicz): Implement this.
+
+        if (database.containsKey(prefix)){
+            Map<String, Object> prefixDatabaseChunk = database.get(prefix);
+            if (prefixDatabaseChunk.containsKey(id)){
+                return (T)prefixDatabaseChunk.get(id);
+            }
+            else {
+                return null;
+            }
+        }
+        else {
+            return null;
+        }
 	}
 
 	@Override
 	public void remove(String prefix, String id) {
 		logger.info("Removing key: " + id + ", prefix:" + prefix);
-		// TODO(kubicz): Implement this.
+
+        if (database.containsKey(prefix)){
+            Map<String, Object> prefixDatabaseChunk = database.get(prefix);
+            if (prefixDatabaseChunk.containsKey(id)){
+                prefixDatabaseChunk.remove(id);
+                saveDatabase();
+            }
+        }
 	}
 
     @Override
     public boolean has(String prefix, String id) {
         logger.info("Checking existence of key: " + id + ", prefix:" + prefix);
-        return false;
-        // TODO(kubicz): Implement this.
+
+        return database.containsKey(prefix) && database.get(prefix).containsKey(id);
     }
 
     @Override
     public int getNextFromSequence(String prefix, String sequenceName) {
         logger.info("Getting sequence: " + prefix + "," + sequenceName);
-        return -1;
-        // TODO(kubicz): Implement this.
+
+        if (!sequences.containsKey(prefix)){
+            sequences.put(prefix, new HashMap<String, Integer>());
+        }
+
+        if (!sequences.get(prefix).containsKey(sequenceName)){
+            sequences.get(prefix).put(sequenceName, 0);
+        }
+
+        int returnVal = sequences.get(prefix).get(sequenceName);
+        sequences.get(prefix).put(sequenceName, returnVal + 1);
+        saveDatabase();
+
+        return returnVal;
+    }
+
+    private void saveDatabase(){
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(DB_FILE_NAME));
+            oos.writeObject(database);
+            oos.writeObject(sequences);
+        } catch (IOException e) {
+            logger.error("Unable to save to " + DB_FILE_NAME);
+        }
+    }
+
+    private void loadDatabase(){
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(DB_FILE_NAME));
+            database = (Map<String, Map<String, Object>>) ois.readObject();
+            sequences = (Map<String, Map<String, Integer>>) ois.readObject();
+        } catch (IOException e) {
+            logger.warn("Unable to load " + DB_FILE_NAME + ". Creating empty database.");
+            database = new HashMap<String, Map<String, Object>>();
+            sequences = new HashMap<String, Map<String, Integer>>();
+        } catch (ClassNotFoundException e) {
+            logger.warn("Unable to deserialize " + DB_FILE_NAME + ". Creating empty database.");
+            database = new HashMap<String, Map<String, Object>>();
+            sequences = new HashMap<String, Map<String, Integer>>();
+        }
     }
 }

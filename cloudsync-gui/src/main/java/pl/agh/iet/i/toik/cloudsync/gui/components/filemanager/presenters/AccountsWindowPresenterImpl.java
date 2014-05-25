5372ebe46e6d9451092c7d166e201d2d37551c65
Daniel Olszowski <olszowski.daniel@gmail.com>
2014-05-26 00:12:23 +0200

    Added loading data when accounts window is opened for the first time.

--- cloudsync-gui/src/main/java/pl/agh/iet/i/toik/cloudsync/gui/components/filemanager/presenters/AccountsWindowPresenterImpl.java
+++ cloudsync-gui/src/main/java/pl/agh/iet/i/toik/cloudsync/gui/components/filemanager/presenters/AccountsWindowPresenterImpl.java
@@ -1,9 +1,13 @@
 package pl.agh.iet.i.toik.cloudsync.gui.components.filemanager.presenters;
 
 import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.vaadin.spring.events.Event;
 import org.vaadin.spring.events.EventBusListenerMethod;
@@ -15,6 +19,7 @@ import pl.agh.iet.i.toik.cloudsync.gui.components.filemanager.views.AccountsWind
 import pl.agh.iet.i.toik.cloudsync.gui.components.filemanager.views.FilesTabSheetView;
 import pl.agh.iet.i.toik.cloudsync.gui.components.presenters.AbstractPresenter;
 import pl.agh.iet.i.toik.cloudsync.logic.Account;
+import pl.agh.iet.i.toik.cloudsync.logic.AccountService;
 
 @Component
 public class AccountsWindowPresenterImpl extends
@@ -25,11 +30,25 @@ public class AccountsWindowPresenterImpl extends
 	
 	private FilesTabSheetView currentFilesTabSheetView; 
 	
+	@Autowired
+	private AccountService accountService;
+
+	private boolean wasOpened = false;
+	
 	@Override
 	@EventBusListenerMethod
 	public void onOpenWindow(Event<OpenAccountsWindowEvent> event) {
 		logger.info("onOpenWindow");
 		currentFilesTabSheetView = event.getPayload().getCaller();
+		
+		if (!wasOpened) {
+			List<Account> accounts = accountService.getAllAccounts();
+			logger.info("adding accounts: " + accounts.size() + " account");
+			for (Account account : accounts) {
+				getComponentView().addAccount(account);
+			}
+			wasOpened = true;
+		}
 		
 		getComponentView().showWindow();
 

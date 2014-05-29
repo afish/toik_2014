c814aded4bd56977a1f8cd70296d1e90ef45f19a
michal3141 <michal3141@op.pl>
2014-05-30 01:23:01 +0200

    ProgressTask getProgress tested

---
+++ cloudsync-onedrive/src/test/pl/agh/iet/i/toik/cloudsync/onedrive/task/ProgressTaskTest.java
@@ -0,0 +1,26 @@
+package pl.agh.iet.i.toik.cloudsync.onedrive.task;
+
+import junit.framework.TestCase;
+
+public class ProgressTaskTest extends TestCase {
+
+    private ProgressTask<String> progressTask;
+
+    public void setUp() throws Exception {
+
+        super.setUp();
+        progressTask = new ProgressTask<>(new ProgressCallable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "e4c5Nf3Nc6";
+            }
+        });
+
+    }
+
+    public void tearDown() throws Exception { }
+
+    public void testGetProgress() throws Exception {
+        assertTrue(progressTask.getProgress() >= 0);
+    }
+}
